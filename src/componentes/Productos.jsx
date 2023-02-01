e4572226e711e95cf8129cb2608c9e42574462d5
Alexander HG <alexanderhuamanguevara@gmail.com>
2023-02-01 17:38:26 -0500

    2

--- src/componentes/Productos.jsx
+++ src/componentes/Productos.jsx
@@ -50,7 +50,7 @@ const Productos = () => {
             <img
               className="border-b-4 border-cyan-600"
               src={dat.img}
-              alt={img1}
+              alt={" IMAGEN DAÑADA"}
             />
 
             <h3 className="text-sm flex justify-center py-1 font-semibold text-gray-700">
